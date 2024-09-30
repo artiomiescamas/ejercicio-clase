c7259bb0ff1a5be83399915ec998043aa2535354
artiomiescamas <iniguez.carrasco.artiom@iescamas.es>
2024-09-30 10:08:20 +0200

    Add files via upload

---
+++ ejerecicios.py
@@ -0,0 +1,8 @@
+#esto es un programa que convierte euros en dolares
+print("dime una cantidad")
+num1 = int(input())
+num2= float(1.10)
+#realizamos la operación 
+total= num1*num2
+
+print("El resultado de sumar",num1,"*",num2, "=",total)
---
+++ suma_dos_numeros.py
@@ -0,0 +1,13 @@
+# esto es un programa que suma dos números
+print("dime un número")
+num1 = int(input())
+
+print("dime otro numero")
+num2= int( input())
+
+#realizamos la operacion de suma 
+
+
+total= num1+num2
+
+print("El resultado de sumar",num1,"+",num2, "=",total)
