d68d6a6f2d2ccd24891d49c0d98eac7b6699e004
artiomiescamas <iniguez.carrasco.artiom@iescamas.es>
2024-10-01 10:49:08 +0200

    Add files via upload

---
+++ ejercicios 2.py
@@ -0,0 +1,7 @@
+#vamos a hacer un programa que solicita el monto de una factura
+print("dime el monto de la factura")
+monto_factura=float(input()) 
+
+propina=monto_factura * 15/100
+
+print("la propina es", propina, "€")
