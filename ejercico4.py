5b39c446d5904e9b34e612b99d9ad1abd1140062
artiom <iniguez.carrasco.artiom@iescamas.es>
2024-10-07 10:06:30 +0200

    Add files via upload

---
+++ ejercico4.py
@@ -0,0 +1,9 @@
+#Este programa se encarga de calcular la media de tres notas
+print("dime tu primera nota")
+num1 =float(input())
+print("dime tu segunda nota")
+num2=float(input())
+print("dime tu tercera nota")
+num3=float(input())
+media= (num1+num2+num3)/3
+print("el resultado de la nota es",media)
